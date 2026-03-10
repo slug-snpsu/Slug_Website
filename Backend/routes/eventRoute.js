add4c72ae1fd21f8f1f8d0448cb1d8d58f97f293
NuthaN-444 <nuthanprasadkg@gmail.com>
2026-03-10 05:36:00 +0530

    backend ---> Event route add

---
+++ Backend/routes/eventRoute.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { getAllEventsController, registrationForEventController, registrationCancelForEventController, addEventController, updateEventController, deleteEventController} = require("../controllers/eventController");
+const { verifyToken } = require("../util/JWT.Token"); 
+const upload = require("../middleware/multer")
+const router = express.Router();
+
+
+// for user and admin [Getting all events]
+router.get("/getAllEvents",getAllEventsController);
+
+
+//user registration
+router.put("/registerForEvents/:event_id",verifyToken,registrationForEventController);
+
+
+//Cancel registration
+router.put("/registerCancelForEvents/:event_id",verifyToken,registrationCancelForEventController);
+
+
+//for admin [ adding events ]
+router.post("/addEvents",verifyToken,upload.single("eventImage"),addEventController);
+
+
+//for admin [ Updating events ]
+router.put("/updateEvents",verifyToken,updateEventController)
+
+
+//for admin [ deleting events ]
+router.delete("/deleteEvents/:_id",verifyToken,deleteEventController);
+
+
+module.exports = router;
